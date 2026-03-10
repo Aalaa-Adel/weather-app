fa28eec4e732bf1973603c5ca71d206b5cd461be
Aalaa Adel <aalaaadel02@gmail.com>
2026-03-10 19:24:00 +0200

    Refine app theme for dark and light mode

--- app/src/main/java/com/example/breez/presentation/theme/Color.kt
+++ app/src/main/java/com/example/breez/presentation/theme/Color.kt
@@ -2,53 +2,66 @@ package com.example.breez.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-/* -------------------- Dark Theme -------------------- */
-val DarkPrimary = Color(0xFF6E56CF)
-val DarkOnPrimary = Color(0xFFFFFFFF)
-
-val DarkSecondary = Color(0xFF9B8CFF)
-val DarkOnSecondary = Color(0xFFFFFFFF)
-
-val DarkBackground = Color(0xFF1D0F4D)
-val DarkSurface = Color(0xFF2B1668)
-val DarkSurfaceVariant = Color(0xFF4A2C88)
-val DarkOnBackground = Color(0xFFF7F4FF)
-val DarkOnSurface = Color(0xFFF7F4FF)
-
-val DarkOutline = Color(0xFF8B7CC7)
-val DarkMutedText = Color(0xFFD5CCFF)
-
-// Accent colors used in weather UI
-val SunYellow = Color(0xFFFFC83D)
-val RainBlue = Color(0xFF42C9FF)
-val CloudWhite = Color(0xFFF3F6FF)
-
-/* Dark gradients */
-val DarkGradientTop = Color(0xFF2D1B7A)
-val DarkGradientBottom = Color(0xFF1A0D46)
-
-val DarkCardGradientStart = Color(0xFF6E56CF)
-val DarkCardGradientEnd = Color(0xFF4A2C88)
-
-/* -------------------- Light Theme -------------------- */
-val LightPrimary = Color(0xFF5D4DD6)
+// Light palette
+val LightPrimary = Color(0xFF6C5CE7)
 val LightOnPrimary = Color(0xFFFFFFFF)
-
-val LightSecondary = Color(0xFF7B6CF3)
+val LightSecondary = Color(0xFF8E7CFF)
 val LightOnSecondary = Color(0xFFFFFFFF)
 
-val LightBackground = Color(0xFFF5F3FF)
+val LightBackground = Color(0xFFF6F4FF)
+val LightOnBackground = Color(0xFF1C1A2E)
+
 val LightSurface = Color(0xFFFFFFFF)
-val LightSurfaceVariant = Color(0xFFE9E3FF)
-val LightOnBackground = Color(0xFF1F1A3D)
-val LightOnSurface = Color(0xFF1F1A3D)
+val LightOnSurface = Color(0xFF1C1A2E)
 
-val LightOutline = Color(0xFFC9C0F5)
-val LightMutedText = Color(0xFF6E6892)
+val LightSurfaceVariant = Color(0xFFEDE9FF)
+val LightOnSurfaceVariant = Color(0xFF5D587A)
 
-/* Light gradients */
-val LightGradientTop = Color(0xFF7B6CF3)
-val LightGradientBottom = Color(0xFFB7A9FF)
+val LightOutline = Color(0xFFD2CCF4)
+val LightOutlineVariant = Color(0xFFE7E2FB)
 
-val LightCardGradientStart = Color(0xFFFFFFFF)
-val LightCardGradientEnd = Color(0xFFF2EEFF)
+val LightPrimaryContainer = Color(0xFFE7E1FF)
+val LightOnPrimaryContainer = Color(0xFF24185F)
+
+val LightSecondaryContainer = Color(0xFFEDE9FF)
+val LightOnSecondaryContainer = Color(0xFF2B2358)
+
+// Dark palette
+val DarkPrimary = Color(0xFFA99BFF)
+val DarkOnPrimary = Color(0xFF1E144E)
+val DarkSecondary = Color(0xFFC4BBFF)
+val DarkOnSecondary = Color(0xFF261E57)
+
+val DarkBackground = Color(0xFF0F0C1F)
+val DarkOnBackground = Color(0xFFF4F2FF)
+
+val DarkSurface = Color(0xFF17132C)
+val DarkOnSurface = Color(0xFFF4F2FF)
+
+val DarkSurfaceVariant = Color(0xFF241D45)
+val DarkOnSurfaceVariant = Color(0xFFC9C2EC)
+
+val DarkOutline = Color(0xFF5C5680)
+val DarkOutlineVariant = Color(0xFF322B57)
+
+val DarkPrimaryContainer = Color(0xFF30246B)
+val DarkOnPrimaryContainer = Color(0xFFE8E2FF)
+
+val DarkSecondaryContainer = Color(0xFF2A2352)
+val DarkOnSecondaryContainer = Color(0xFFEAE5FF)
+
+// Weather accents
+val SunAccent = Color(0xFFFFC857)
+val RainAccent = Color(0xFF69D2FF)
+val CloudAccent = Color(0xFFE9EEFF)
+val StormAccent = Color(0xFF9D8CFF)
+val WindAccent = Color(0xFFCFE5FF)
+
+// Home gradients
+val DarkGradientTop = Color(0xFF33208F)
+val DarkGradientMid = Color(0xFF1E1456)
+val DarkGradientBottom = Color(0xFF100C23)
+
+val LightGradientTop = Color(0xFF9488FF)
+val LightGradientMid = Color(0xFFC5BCFF)
+val LightGradientBottom = Color(0xFFF7F4FF)
